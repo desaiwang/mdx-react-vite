8403c5b400ee8ca401bf1a25cd21580e1cf6d833
Desai Wang <dw584@cornell.edu>
2024-11-29 12:26:56 +0800

    rollback to hashrouter
    browser router still doesn't work

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import BlogList from "./BlogList";
 import BlogPost from "./BlogPost";
 function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<BlogList />} />
             <Route path="/:slug" element={<BlogPost />} />
             <Route path="/*" element={<BlogList />} />{" "}
             {/* This is a catch-all route */}
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </>
   );
